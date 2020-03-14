defcd3806419809b2e5b6804b46118b32215b491
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-14 23:00:47 +0000

    Updates

---
+++ precache-manifest.fd2c7f571e16d81e4c2b26cd567542c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41b04fc200a76376e5a686027531fc1",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "902dc036ade50f659f76",
+    "url": "/react_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48a45415828b4de57519",
+    "url": "/react_test/static/js/2.0f273958.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_test/static/js/2.0f273958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902dc036ade50f659f76",
+    "url": "/react_test/static/js/main.52653fb2.chunk.js"
+  },
+  {
+    "revision": "2540bfd26d0b66c8bc5c",
+    "url": "/react_test/static/js/runtime-main.5e96ea67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
